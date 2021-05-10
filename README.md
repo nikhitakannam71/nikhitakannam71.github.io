90ebf476613aa00164e68300f2b111ea91eb1173
nikhita.kannam71@qmail.cuny.edu <nikhita.kannam71@qmail.cuny.edu>
2021-05-10 00:45:12 -0400

    Dashboard #2

--- README.md
+++ README.md
@@ -1,4 +1,7 @@
-﻿
+
+
+# nikhitakannam71.github.io
+
 
 This dashboard has projects that I've worked on in CS355, including projects from LinkedIn Learning, FreeCodeCamp and the Term Project, which is not yet finished. 
 
@@ -6,5 +9,26 @@ This dashboard has projects that I've worked on in CS355, including projects fro
 **Link to the website:**  [nikhitakannam71.github.io]
 
 **Link to the repo:** https://github.com/nikhitakannam71/nikhitakannam71.github.io
+
+
+## **LinkedIn Learning:**
+
+ - The projects from Linked Learning include the Resume Project, which was from the CSS essential training.  
+ - The Typing Test app is from the
+   JavaScript Essential Training . 
+ - The Chat-App is from the Learning-Node.js course and works with backend and databases.
+
+
+
+## FreeCodeCamp:
+
+
+The projects from here are from FreeCodeCamp and include Timestamp Microservice, Exercise Tracker, URL Shortener Microservice and Request Header Parser. 
+
+## Term Project:
+The term project is the QCFirst website and so far it include all the HTML and CSS. It also includes the User Managment of the project. 
+
+
+
 
 
---
+++ project-exercisetracker/server.js
@@ -0,0 +1,91 @@
+const express = require('express')
+const app = express()
+const bodyParser = require('body-parser')
+const cors = require('cors')
+const mongoose = require('mongoose')
+require('dotenv').config()
+const shortId = require("shortid")
+
+
+let sub = { useNewUrlParser: true, useUnifiedTopology: true }
+mongoose.connect('mongodb+srv://nkannam1:Nikki25first@cluster0.ouxc5.mongodb.net/ExerciseTracker?retryWrites=true&w=majority', sub);
+
+const NewUserSch = mongoose.Schema({
+  description:{type: String, required: true},
+  duration: {type: Number, required: true},
+  date: String
+});
+const NewSchema = mongoose.Schema({
+  username:{type: String, required: true},
+  log: [NewUserSch]
+});
+let Exe = new  mongoose.model('Exe', NewSchema);
+let Tra = new mongoose.model('Tra', NewUserSch);
+
+app.use(cors())
+app.use(express.json());
+app.use(express.urlencoded());
+app.use(express.static('public'))
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/views/')
+});
+
+app.post("/api/users", function (req,res){
+  let newUser = new Exe({username: req.body.username});
+  newUser.save();
+  return res.json({'username': newUser.username, '_id': newUser.id}) 
+})
+app.get("/api/users", function (req, res){
+  Exe.find({}, function(err, data){ 
+    res.json(data);
+  })
+})
+
+
+app.post("/api/users/:_id/exercises", function(req, res){
+  let dur = req.body.duration;
+  let da = req.body.date;
+  var newTracker = new Tra({description: req.body.description, 
+  duration: dur,
+  date: da
+  })
+  if(!newTracker.date){newTracker.date = new Date().toISOString().substring(0,10);}
+  newTracker.save();
+  let y = Exe.findByIdAndUpdate(req.params._id,
+      {$push: {log: newTracker}},
+      {new: true},function(err, data){
+        let first = {}
+        first["username"] = data.username
+        first["description"] = req.body.description
+        first["duration"] = newTracker.duration
+        first["_id"] = data._id
+        first["date"] = new Date(newTracker.date).toDateString(); 
+        res.json(first);
+      })
+})
+
+
+
+app.get("/api/users/:_id/logs", function(req,res){
+  
+  let y = Exe.findById(req.params._id, function(err, data){
+      let next = {}
+      next["_id"] = data._id
+      next["username"] = data.username
+      end = req.query.to ? new Date(req.query.to) : new Date();
+      start = req.query.from ? new Date(req.query.from) : new Date(0);
+      data.log = data.log.filter((item) =>{
+        let next = new Date(item.date).getTime();
+        return next >= start && next <= end;
+      })
+      next["log"] = data.log.slice(0, req.query.limit);
+      next["count"] = data.log.length;
+      
+      res.json(next);
+      
+  })
+})
+
+const listener = app.listen(process.env.PORT || 3000, () => {
+  console.log('Your app is listening on port ' + listener.address().port)
+})
